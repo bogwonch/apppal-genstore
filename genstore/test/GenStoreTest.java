65bc22848b7d2a1ff3177076464e27423c3b535f
Joseph Hallett <bogwonch@bogwonch.net>
2015-06-25 16:05:28 +0100

    Adding JUnit

---
+++ genstore/test/GenStoreTest.java
@@ -0,0 +1,11 @@
+package genstore.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class GenStoreTest {
+  @Test
+  public void sanity() {
+    assertEquals(1, 1);
+  }
+}
